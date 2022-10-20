ef2e7bf2e20bcea1de85c8cfe255b9e6260d2e73
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-20 20:24:09 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,38 @@
 #include "main.h"
 /**
+  * specifier_check - check specifier
+  * @format: format specifier
+  *
+  * Return: pointer to function
+  */
+static int (*specifier_check(const char *format))(va_list)
+{
+		unsigned int i;
+
+		print_t p[] = {
+			{"c", print_c},
+			{"s", print_s},
+			{"i", print_i},
+			{"d", print_d},
+			{"u", print_u},
+			{"b", print_b},
+			{"o", print_o},
+			{"x", print_x},
+			{"X", print_X},
+			{"p", print_p},
+			{"S", print_S},
+			{"r", print_r},
+			{"R", print_R},
+			{NULL, NULL}
+		};
+		for (i = 0; p[i].t != NULL; i++)
+		{
+			if (*(p[i].t) == *format)
+				break;
+		}
+		return (p[i].f);
+}
+/**
   * _printf - function that produces output according to a format.
   * @format: Character string of type
   *
@@ -7,13 +40,40 @@
   */
 int _printf(const char *format, ...)
 {
+	unsigned int i = 0, count = 0;
 	va_list valist;
-	int i;
+	int (*f)(va_list);
 
-	va_start(valist, char);
-	
-	
+	if (format == NULL)
+		return (-1);
 
-	return (0);
+	va_start(valist, format);
+
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = specifier_check(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
- 
--- main.h
+++ main.h
@@ -4,7 +4,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _printf(const char *format, ...);
+static int (*specifier_check(const char *format))(va_list);
 
 #endif
